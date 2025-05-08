821febb1931280c472a147be3233816fddfe2c2a
Melody5star <34328230+Melody5star@users.noreply.github.com>
2025-05-08 15:21:43 -0400

    Create ext.config.ts

---
+++ ext.config.ts
@@ -0,0 +1,26 @@
+// next.config.ts
+import type {NextConfig} from 'next';
+
+const nextConfig: NextConfig = {
+  output: 'export',
+  /* config options here */
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    unoptimized: true, // Add this line to disable Image Optimization API
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+};
+
+export default nextConfig;
