93ecac006dc577ad8313f6926cb3c6fb98bd7b0a
Melody5star <34328230+Melody5star@users.noreply.github.com>
2025-05-08 18:09:14 -0400

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
-  output: 'export',
+     //  / * output: 'export', */  commenting for dynamic node deploy.-To host in Node server, I am removing output: 'export', line in next.config.ts To allow the app to run as a Node.js server, please remove the line output: 'export' 
   /* config options here */
   typescript: {
     ignoreBuildErrors: true,
